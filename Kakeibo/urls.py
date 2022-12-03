81660f0d5b72f791fd05f4ddeb5c98c3c7b8b528
Yusuke Kimura <kippi0326@gmail.com>
2022-12-03 17:56:12 +0900

    add：支出一覧ページの作成のファイル追加及び修正

--- Kakeibo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'Kakeibo'
-urlpatterns = [
-    path('index/', views.IndexView.as_view(), name='index'),
-]
---
+++ kakeibo/views.py
@@ -0,0 +1,8 @@
+from django.views import generic
+from .models import Payment, PaymentCategory, Income, IncomeCategory
+
+
+class PaymentList(generic.ListView):
+    template_name = 'kakeibo/payment_list.html'
+    model = Payment
+    ordering = '-date'
